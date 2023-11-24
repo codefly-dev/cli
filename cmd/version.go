211353a05845324078bb2cd3317239a566fafbf1
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2023-11-24 12:24:10 +0100

    feat: version of cli

--- cmd/version.go
+++ cmd/version.go
@@ -1,14 +1,36 @@
 package cmd
 
 import (
+	"embed"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
+	"io/fs"
+	"log"
 )
+
+type Information struct {
+	Version string `json:"version"`
+}
 
 // VersionCmd represents the build command
 var VersionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Version of codefly",
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Println("0.0.1")
+		data, err := fs.ReadFile(info, "info.yaml")
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+
+		// Unmarshal YAML into a struct
+		var info Information
+		err = yaml.Unmarshal(data, &info)
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+		cmd.Println(info.Version)
 	},
 }
+
+//go:embed info.yaml
+var info embed.FS
