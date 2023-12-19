3d443936cae65b9e59eb9dbc0b0fb6448bd7be8b
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2023-12-19 10:21:22 +0100

    clean: dependency

--- pkg/partial/partial.go
+++ pkg/partial/partial.go
@@ -1,102 +1,103 @@
 package partial
 
-import (
-	"context"
-	"fmt"
-
-	"github.com/codefly-dev/cli/pkg/application"
-	"github.com/codefly-dev/cli/pkg/cli/display"
-	"github.com/codefly-dev/core/configurations"
-	"github.com/codefly-dev/core/shared"
-	"github.com/codefly-dev/core/wool"
-)
-
-type Partial struct {
-	Configuration *configurations.Partial
-	Applications  []*application.Application
-}
-
-func NewPartial(ctx context.Context, project *configurations.Project, partial *configurations.Partial, mode application.Mode) (*Partial, error) {
-	w := wool.Get(ctx).In("partial.NewPartial<%s>", partial.Name)
-	configurations.SetMode(configurations.ModePartial)
-	display.PartialLoading(partial)
-	p := &Partial{Configuration: partial}
-	for _, name := range partial.Applications {
-		logger.Debuf("Loading application: %s", name)
-		// Get config
-		//config, err := configurations.LoadApplicationFromName(name)
-		//if err != nil {
-		//	return nil, logger.Wrapf(err, "failed to load application configuration: %s", name)
-		//}
-		//app, err := application.Load(project, config, mode)
-		//if err != nil {
-		//	return nil, logger.Wrapf(err, "failed to load application: %s", config)
-		//}
-		//err = p.Add(app)
-		//if err != nil {
-		//	return nil, logger.Wrapf(err, "failed to add application: %s", config)
-		//}
-
-	}
-
-	return p, nil
-}
-
-func (p *Partial) Configure(ctx context.Context) error {
-	var errs []error
-	for _, app := range p.Applications {
-		if err := app.Configure(ctx); err != nil {
-			errs = append(errs, err)
-		}
-	}
-	return shared.MultiErrors(errs...)
-}
-
-func (p *Partial) Add(app *application.Application) error {
-	if app == nil {
-		return fmt.Errorf("got a nil app")
-	}
-	p.Applications = append(p.Applications, app)
-	return nil
-}
-
-func (p *Partial) Stop(ctx context.Context) error {
-	var errs []error
-	for _, app := range p.Applications {
-		if err := app.Stop(ctx); err != nil {
-			errs = append(errs, err)
-		}
-	}
-	return shared.MultiErrors(errs...)
-}
-
-func (p *Partial) Sync(ctx context.Context) error {
-	var errs []error
-	for _, app := range p.Applications {
-		if err := app.Sync(ctx); err != nil {
-			errs = append(errs, err)
-		}
-	}
-	return shared.MultiErrors(errs...)
-}
-
-func (p *Partial) Build(ctx context.Context) error {
-	var errs []error
-	for _, app := range p.Applications {
-		if err := app.Build(ctx); err != nil {
-			errs = append(errs, err)
-		}
-	}
-	return shared.MultiErrors(errs...)
-}
-
-func (p *Partial) Deploy(ctx context.Context, env *configurations.Environment) error {
-	var errs []error
-	for _, app := range p.Applications {
-		if err := app.Deploy(ctx, env); err != nil {
-			errs = append(errs, err)
-		}
-	}
-	return shared.MultiErrors(errs...)
-
-}
+//
+//import (
+//	"context"
+//	"fmt"
+//
+//	"github.com/codefly-dev/cli/pkg/application"
+//	"github.com/codefly-dev/cli/pkg/cli/display"
+//	"github.com/codefly-dev/core/configurations"
+//	"github.com/codefly-dev/core/shared"
+//	"github.com/codefly-dev/core/wool"
+//)
+//
+//type Partial struct {
+//	Configuration *configurations.Partial
+//	Applications  []*application.Application
+//}
+//
+//func NewPartial(ctx context.Context, project *configurations.Project, partial *configurations.Partial, mode application.Mode) (*Partial, error) {
+//	w := wool.Get(ctx).In("partial.NewPartial<%s>", partial.Name)
+//	configurations.SetMode(configurations.ModePartial)
+//	display.PartialLoading(partial)
+//	p := &Partial{Configuration: partial}
+//	for _, name := range partial.Applications {
+//		logger.Debuf("Loading application: %s", name)
+//		// Get config
+//		//config, err := configurations.LoadApplicationFromName(name)
+//		//if err != nil {
+//		//	return nil, logger.Wrapf(err, "failed to load application configuration: %s", name)
+//		//}
+//		//app, err := application.Load(project, config, mode)
+//		//if err != nil {
+//		//	return nil, logger.Wrapf(err, "failed to load application: %s", config)
+//		//}
+//		//err = p.Add(app)
+//		//if err != nil {
+//		//	return nil, logger.Wrapf(err, "failed to add application: %s", config)
+//		//}
+//
+//	}
+//
+//	return p, nil
+//}
+//
+//func (p *Partial) Configure(ctx context.Context) error {
+//	var errs []error
+//	for _, app := range p.Applications {
+//		if err := app.Configure(ctx); err != nil {
+//			errs = append(errs, err)
+//		}
+//	}
+//	return shared.MultiErrors(errs...)
+//}
+//
+//func (p *Partial) Add(app *application.Application) error {
+//	if app == nil {
+//		return fmt.Errorf("got a nil app")
+//	}
+//	p.Applications = append(p.Applications, app)
+//	return nil
+//}
+//
+//func (p *Partial) Stop(ctx context.Context) error {
+//	var errs []error
+//	for _, app := range p.Applications {
+//		if err := app.Stop(ctx); err != nil {
+//			errs = append(errs, err)
+//		}
+//	}
+//	return shared.MultiErrors(errs...)
+//}
+//
+//func (p *Partial) Sync(ctx context.Context) error {
+//	var errs []error
+//	for _, app := range p.Applications {
+//		if err := app.Sync(ctx); err != nil {
+//			errs = append(errs, err)
+//		}
+//	}
+//	return shared.MultiErrors(errs...)
+//}
+//
+//func (p *Partial) Build(ctx context.Context) error {
+//	var errs []error
+//	for _, app := range p.Applications {
+//		if err := app.Build(ctx); err != nil {
+//			errs = append(errs, err)
+//		}
+//	}
+//	return shared.MultiErrors(errs...)
+//}
+//
+//func (p *Partial) Deploy(ctx context.Context, env *configurations.Environment) error {
+//	var errs []error
+//	for _, app := range p.Applications {
+//		if err := app.Deploy(ctx, env); err != nil {
+//			errs = append(errs, err)
+//		}
+//	}
+//	return shared.MultiErrors(errs...)
+//
+//}
--- pkg/partial/partial_run.go
+++ pkg/partial/partial_run.go
@@ -1,35 +1,36 @@
 package partial
 
-import (
-	"context"
-	"sync"
-
-	"github.com/codefly-dev/cli/pkg/application"
-	"github.com/codefly-dev/core/wool"
-)
-
-// Run runs the applications of the partial
-// Each application run is blocking so we need to go-routine them
-func (p *Partial) Run(ctx context.Context) error {
-	w := wool.Get(ctx).In("partial.Run")
-	for _, app := range p.Applications {
-		err := app.Configure(ctx)
-		if err != nil {
-			return logger.Wrapf(err, "cannot configure application")
-		}
-	}
-	var wg sync.WaitGroup
-	errs := make(chan error)
-	for _, app := range p.Applications {
-		logger.Debuf("Loading application: %s", app.Configuration.Name)
-		wg.Add(1)
-		go func(app *application.Application) {
-			defer wg.Done()
-			if err := app.Run(ctx); err != nil {
-				errs <- err
-			}
-		}(app)
-	}
-	wg.Wait()
-	return nil
-}
+//
+//import (
+//	"context"
+//	"sync"
+//
+//	"github.com/codefly-dev/cli/pkg/application"
+//	"github.com/codefly-dev/core/wool"
+//)
+//
+//// Run runs the applications of the partial
+//// Each application run is blocking so we need to go-routine them
+//func (p *Partial) Run(ctx context.Context) error {
+//	w := wool.Get(ctx).In("partial.Run")
+//	for _, app := range p.Applications {
+//		err := app.Configure(ctx)
+//		if err != nil {
+//			return logger.Wrapf(err, "cannot configure application")
+//		}
+//	}
+//	var wg sync.WaitGroup
+//	errs := make(chan error)
+//	for _, app := range p.Applications {
+//		logger.Debuf("Loading application: %s", app.Configuration.Name)
+//		wg.Add(1)
+//		go func(app *application.Application) {
+//			defer wg.Done()
+//			if err := app.Run(ctx); err != nil {
+//				errs <- err
+//			}
+//		}(app)
+//	}
+//	wg.Wait()
+//	return nil
+//}
