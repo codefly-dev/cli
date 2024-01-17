42202a03a6fde4ef99a8f5f54b55d8bdbb0b8be0
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2024-01-17 12:52:30 +0100

    feat: communicate input

---
+++ pkg/cli/communicate/input.go
@@ -0,0 +1,20 @@
+package communicate
+
+import (
+	"github.com/codefly-dev/cli/pkg/cli"
+	"github.com/codefly-dev/cli/pkg/cli/models"
+	agentv0 "github.com/codefly-dev/core/generated/go/services/agent/v0"
+)
+
+func Input(msg *agentv0.Message, c *agentv0.Input) (*agentv0.Answer, error) {
+	cli.Header(2, msg.Description)
+	// TODO: Deal with int
+	input := models.Input(msg.Message, c.GetStringDefault())
+	return &agentv0.Answer{
+		Value: &agentv0.Answer_Input{
+			Input: &agentv0.InputAnswer{
+				Answer: &agentv0.InputAnswer_StringValue{StringValue: input},
+			},
+		},
+	}, nil
+}
